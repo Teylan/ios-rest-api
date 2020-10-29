402de130c4899bec9c6309280d114501a018da57
Grant <grantsrockets@yahoo.com>
2020-10-28 21:45:43 -0700

    done

--- ios-rest-api/ViewController.swift
+++ ios-rest-api/ViewController.swift
@@ -8,16 +8,27 @@
 
 import UIKit
 
-    let DomainURL = "http://216.186.69.45/services/device/"
+    let DomainURL = "https://www.orangevalleycaa.org/api/"
+
+  
     
-    class User {
+class Music : Codable{
         
-        static func fetch(){
-            let URLstring = DomainURL + "users/"
+    var id : String?
+    var music_url : String?
+    var name : String?
+    var description : String?
+    
+    static func fetch(withId id: Int, completionHandler: @escaping (Music)->Void){
+            let URLstring = DomainURL + "music/\(id)"
             if let url = URL.init(string: URLstring){
-                let task = URLSession.shared.dataTask(with: url, completionHandler:
-                    //TODO: Add closure
-                )
+                let task = URLSession.shared.dataTask(with: url, completionHandler:{(data, response, error) in
+                    print(String.init(data: data!, encoding: .ascii) ?? "no data")
+                    if let newUser = try? JSONDecoder().decode(Music.self, from: data!){
+                        print(newUser.music_url ?? "null")
+                        completionHandler(newUser)
+                        }
+                    })
                 task.resume()
             }
         }
@@ -31,7 +42,12 @@ class ViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        User.fetch()
+        Music.fetch(withId: 1) { (newUser) in
+            print(newUser.name ?? "no name")
+            if let userData = try? JSONEncoder().encode(newUser){
+                print(userData)
+            }
+        }
     }
 
 
